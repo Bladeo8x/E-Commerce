b5fd805555a7ae867b5e9e8cef1340e83a828a9d
Bladeo8x <gerardo50_@hotmail.com>
2023-05-25 14:15:02 +0700

    uploading app

---
+++ e-commerce/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+      <nav>
+        <h3>ECOFI</h3>
+        <div>
+          <button>Ice Coffee</button>
+          <button>Hot Coffee</button>
+          <button>Ice Tea</button>
+          <button>Hot Tea</button>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
